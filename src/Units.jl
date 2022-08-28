e4401a79e88c0ffa001077bf611c5674be90e7b3
Brayden Barton <brayden.barton@usu.edu>
2022-08-28 13:55:56 -0600

    Create initial units

--- src/Units.jl
+++ src/Units.jl
@@ -1,5 +1,28 @@
 module Units
+export units
+# This package contains some units for convenient global conversion
+# Length
+m       = 1.
+km      = 1e3
 
-# Write your package code here.
+# Time
+s       = 1.
+min     = 60.
+hr      = 360.
+day     = 86400.
+year    = 365.25*day
 
+# Mass
+kg      = 1.
+g       = 1e-3
+lbm     = 0.4535924
+slug    = 32.17405*lbm
+
+# Collecting and exporting data
+unitnames = (
+    :m, :km,
+    :s, :min, :hr, :day, :year,
+    :kg, :g, :lbm, :slug
+)
+units = NamedTuple{unitnames}(eval.(unitnames))
 end
